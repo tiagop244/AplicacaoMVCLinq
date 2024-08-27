8c2ab541c0ef26bbe3adb508c82c0d31ea7674a6
Tiago Pereira <tiagopereiraduarte8@gmail.com>
2024-08-27 17:05:56 -0300

    Adicionar arquivos de projeto.

---
+++ AplicacaoMVCLinq/AplicacaoMVCLinq/Controllers/ClienteController.cs
@@ -0,0 +1,27 @@
+﻿using AplicacaoMVCLinq.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace AplicacaoMVCLinq.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Cliente> listaCliente = new List<Cliente>();
+
+            Cliente c1 = new Cliente(1, "Alice", "alice@teste.com", "1234-5678", new DateOnly(2000, 6, 1));
+            listaCliente.Add(c1);
+            Cliente c2 = new Cliente(2, "Alice", "alice@teste.com", "1234-5678", new DateOnly(2000, 6, 1));
+            listaCliente.Add(c2);
+            Cliente c3 = new Cliente(3, "Alice", "alice@teste.com", "1234-5678", new DateOnly(2000, 6, 1));
+            listaCliente.Add(c3);
+            Cliente c4 = new Cliente(4, "Alice", "alice@teste.com", "1234-5678", new DateOnly(2000, 6, 1));
+            listaCliente.Add(c4);
+            Cliente c5 = new Cliente(5, "Alice", "alice@teste.com", "1234-5678", new DateOnly(2000, 6, 1));
+            listaCliente.Add(c5);
+
+            return View(listaCliente);
+        }
+    }
+}
---
+++ AplicacaoMVCLinq/AplicacaoMVCLinq/Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿namespace AplicacaoMVCLinq.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public DateOnly DataNasc { get; set; }
+
+        public Cliente(int id, string nome, string email, string telefone, DateOnly dataNasc)
+        {
+            this.ClienteId = id;
+            this.Nome = nome;
+            this.Email = email;
+            this.Telefone = telefone;
+            this.DataNasc = dataNasc;
+        }
+    }
+}
